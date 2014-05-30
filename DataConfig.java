48c4778f2e8e4b090028ae235d8a71c248aef931
Scott Cagno <scottiecagno@gmail.com>
2014-05-30 01:22:40 -0400

    Create DataConfig.java

---
+++ DataConfig.java
@@ -0,0 +1,52 @@
+package $PACKAGE.config;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan("$PACKAGE")
+@PropertySource("classpath:persistence.properties")
+@EnableJpaRepositories("$PACKAGE.data")
+public class DataConfig {
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan("$PACKAGE.domain");
+		final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+		return em;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.pass"));
+		return dataSource;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(final EntityManagerFactory emf) {
+		final JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	final Properties additionalProperties() {
+		final Properties hibernateProperties = new Properties();
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+		hibernateProperties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+		return hibernateProperties;
+	}
+}
