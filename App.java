df8fd870238eff77fcb5b1ab297287a09c185065
Scott Cagno <scottiecagno@gmail.com>
2014-05-30 01:20:36 -0400

    Create App.java

---
+++ App.java
@@ -0,0 +1,11 @@
+package $PACKAGE
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application  {
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Application.class);
+		app.run(args);
+	}
+}
