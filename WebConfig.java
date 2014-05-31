92488f456ae9a7560717a6d0c9e6179b2e475a71
Scott Cagno <scottiecagno@gmail.com>
2014-05-30 20:49:06 -0400

    Update WebConfig.java

--- WebConfig.java
+++ WebConfig.java
@@ -1,5 +1,9 @@
 package $PACKAGE.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
