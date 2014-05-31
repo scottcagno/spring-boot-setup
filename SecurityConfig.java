0a25e562b9e249b0a7d4c79042a39eb1443d9617
Scott Cagno <scottiecagno@gmail.com>
2014-05-30 20:48:25 -0400

    Update SecurityConfig.java

--- SecurityConfig.java
+++ SecurityConfig.java
@@ -1,5 +1,14 @@
 package $PACKAGE.config;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 @Configuration
 @EnableWebMvcSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
