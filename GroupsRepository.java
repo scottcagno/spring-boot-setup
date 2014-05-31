e4afaee891225cd959423920543c619aa5c8cf0c
Scott Cagno <scottiecagno@gmail.com>
2014-05-30 20:54:26 -0400

    Update GroupsRepository.java

--- GroupsRepository.java
+++ GroupsRepository.java
@@ -1,6 +1,10 @@
 package $PACKAGE.data;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import $PACKAGE.domain.Groups;
 
 @Repository
-public interface GroupsRepository extends JpaRepository<Groups,Long> {}
+public interface GroupsRepository extends JpaRepository<Groups,Long> {
+  
+}
