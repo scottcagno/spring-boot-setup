d1b7d5791e3704d5dc0bef5b4c9623353a328861
Scott Cagno <scottiecagno@gmail.com>
2014-05-30 20:45:19 -0400

    Update App.java

--- App.java
+++ App.java
@@ -1,11 +1,16 @@
 package $PACKAGE;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application  {
+public class App  {
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Application.class);
+		SpringApplication app = new SpringApplication(App.class);
 		app.run(args);
 	}
 }
